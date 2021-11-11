6691b56f9b0dbf53e9deb0d45ef6fc8b65f44c85
chrs-m <mister.ch@gmail.com>
2021-11-11 08:55:01 +0100

    add scrollbar status and JS

---
+++ app.js
@@ -0,0 +1,13 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  let height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
