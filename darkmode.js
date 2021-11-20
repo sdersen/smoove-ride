d7d4bf8d170a7c505b0799b640a5e1e567d5e2b5
Sofia Dersén <Sofia_dersen@hotmail.com>
2021-11-20 17:01:57 +0100

    Toggle switch fixad

--- darkmode.js
+++ darkmode.js
@@ -1,20 +1,20 @@
-const darkSwitch = document.querySelector(".switch");
+const darkSwitch = document.querySelector(".switch input");
 const headline = document.querySelector(".title");
 const titleText = document.querySelector(".phrase");
 const SignUpbutton = document.querySelector(".sign-up-btn");
-const signUpP = document.querySelector('.btn-container p');
-const hiddenCar = document.querySelector('.hidden-car');
-const heroBackround = document.querySelector('.hero-section');
+const signUpP = document.querySelector(".btn-container p");
+const hiddenCar = document.querySelector(".hidden-car");
+const heroBackround = document.querySelector(".hero-section");
 // const burgerDiv = document.querySelector('.line1 .line2 .line3 ');
 
 darkSwitch.addEventListener("click", () => {
   headline.classList.toggle("white-text");
   titleText.classList.toggle("white-text");
-  signUpP.classList.toggle("white-text")
+  signUpP.classList.toggle("white-text");
   SignUpbutton.classList.toggle("poo");
-  hiddenCar.classList.toggle('hidden');
-  heroBackround.classList.toggle('bg-black');
+  hiddenCar.classList.toggle("hidden");
+  heroBackround.classList.toggle("bg-black");
   // burgerDiv.classList.toggle('bg-white');
-  event.preventDefault();
+  // event.preventDefault();
   console.log("Toggled");
 });
