1fb11f82149ccb82f017382d81e6e396febf2e22
chrs-m <mister.ch@gmail.com>
2021-11-22 09:57:07 +0100

    clean up code

--- JS/darkmode.js
+++ JS/darkmode.js
@@ -1,20 +1,19 @@
 const darkSwitch = document.querySelector(".switch input");
 const headline = document.querySelector(".title");
 const titleText = document.querySelector(".phrase");
-const SignUpbutton = document.querySelector(".sign-up-btn");
+const signUpBtn = document.querySelector("sign-up-btn");
 const signUpP = document.querySelector(".btn-container p");
 const hiddenCar = document.querySelector(".hidden-car");
 const heroBackround = document.querySelector(".hero-section");
+
 // const burgerDiv = document.querySelector('.line1 .line2 .line3 ');
 
 darkSwitch.addEventListener("click", () => {
   headline.classList.toggle("white-text");
   titleText.classList.toggle("white-text");
   signUpP.classList.toggle("white-text");
-  SignUpbutton.classList.toggle("poo");
+  signUpBtn.classList.toggle("white-text");
   hiddenCar.classList.toggle("hidden");
   heroBackround.classList.toggle("bg-black");
-  // burgerDiv.classList.toggle('bg-white');
-  // event.preventDefault();
-  console.log("Toggled");
+  // console.log("Toggled");
 });
