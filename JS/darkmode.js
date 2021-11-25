b1b13ffe980626a37e17d1d36542738dac5e7530
chrs-m <mister.ch@gmail.com>
2021-11-25 12:00:22 +0100

    Hero section DONE desk/mob

--- JS/darkmode.js
+++ JS/darkmode.js
@@ -5,6 +5,10 @@ const signUpBtn = document.querySelector(".sign-up-btn");
 const signUpParagraph = document.querySelector(".btn-container p");
 const hiddenCar = document.querySelector(".hidden-car");
 const heroBackround = document.querySelector(".hero-section");
+const burgerDivs = document.querySelectorAll(".burger div");
+const navTags = document.querySelectorAll(".nav-links a");
+const logoPaths = document.querySelectorAll(".logo path");
+const darkmodeText = document.querySelector(".darkmode-text");
 
 // const burgerDiv = document.querySelector('.line1 .line2 .line3 ');
 
@@ -15,5 +19,14 @@ darkSwitch.addEventListener("click", () => {
   signUpBtn.classList.toggle("white-btn");
   hiddenCar.classList.toggle("hidden");
   heroBackround.classList.toggle("bg-black");
-  // console.log("Toggled");
+  burgerDivs.forEach((burgerDiv) => {
+    burgerDiv.classList.toggle("white-menu");
+  });
+  navTags.forEach((navTag) => {
+    navTag.classList.toggle("white-text");
+  });
+  logoPaths.forEach((logoPath) => {
+    logoPath.classList.toggle("fill-color");
+  });
+  darkmodeText.classList.toggle("white-text");
 });
